779a1a4c596a0bc2fc7f1ad668d86acb312e7ec7
Youndge <18610457408@163.com>
2019-10-05 21:51:28 +0800

    ORM:insert data

--- controllers/default.go
+++ controllers/default.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"myBeego/models"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
 )
 
 type MainController struct {
@@ -9,6 +12,21 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	/*1.Generate one ORM object*/
+	o := orm.NewOrm()
+	/*2.Generate one object needed inserted*/
+	user := models.User{}
+	/*3.Set values for struct*/
+	user.Name = "Tony"
+	user.Pwd = "123"
+	/*4.insert data*/
+	_, err := o.Insert(&user)
+	if nil != err {
+		beego.Info("Insert Fail! ", err)
+		return
+	}
+	beego.Info("Insert Success!")
+
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
