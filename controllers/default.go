e087fbe1e2365021a581a55e08e405e0d405953f
Youndge <18610457408@163.com>
2019-10-05 22:29:36 +0800

    ORM:delete data

--- controllers/default.go
+++ controllers/default.go
@@ -12,21 +12,21 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	/*ORM Insert*/
-	/*1.Generate one ORM object*/
-	o := orm.NewOrm()
-	/*2.Generate one object needed inserted*/
-	user := models.User{}
-	/*3.Set the value you want to insert*/
-	user.Name = "Tony"
-	user.Pwd = "123"
-	/*4.insert data*/
-	_, err := o.Insert(&user)
-	if nil != err {
-		beego.Info("Insert Fail! ", err)
-		return
-	}
-	beego.Info("Insert Success!")
+	// /*ORM Insert*/
+	// /*1.Generate one ORM object*/
+	// o := orm.NewOrm()
+	// /*2.Generate one object needed inserted*/
+	// user := models.User{}
+	// /*3.Set the value you want to insert*/
+	// user.Name = "Tony"
+	// user.Pwd = "123"
+	// /*4.insert data*/
+	// _, err := o.Insert(&user)
+	// if nil != err {
+	// 	beego.Info("Insert Fail! ", err)
+	// 	return
+	// }
+	// beego.Info("Insert Success!")
 
 	/*ORM Query*/
 	/*1.Generate one ORM object*/
@@ -52,22 +52,36 @@ func (c *MainController) Get() {
 	user2 := models.User{}
 	/*3.Set the value you want to uodate*/
 	user2.Id = 1 /*update by Id*/
-	//user2.Name = "Tony" /*read by Name*/
 	/*4.Find data & Update it*/
 	err2 := o2.Read(&user2) /*read by Id*/
-	//err1 := o1.Read(&user1, "Name") /*read by Name*/
 	if nil != err2 {
-		beego.Info("Update Fail! ", err1)
+		beego.Info("Update Fail! ", err2)
 		return
 	} else {
 		user2.Name = "Tom"
 		user2.Pwd = "222"
-		_, err = o2.Update(&user2)
-		if nil != err {
-			beego.Info("Update Fail! ", err)
+		_, err2 = o2.Update(&user2)
+		if nil != err2 {
+			beego.Info("Update Fail! ", err2)
 		}
 	}
 	beego.Info("Update Success!")
+
+	/*ORM Delete*/
+	/*1.Generate one ORM object*/
+	o3 := orm.NewOrm()
+	/*2.Generate one object need to delete*/
+	user3 := models.User{}
+	/*3.Set the value you want to delete*/
+	user3.Id = 1 /*update by Id*/
+	//user2.Name = "Tony" /*read by Name*/
+	/*4.Find data & Update it*/
+	_, err3 := o3.Delete(&user3) /*read by Id*/
+	if nil != err3 {
+		beego.Info("Delete Fail! ", err3)
+		return
+	}
+	beego.Info("Delete Success!")
 
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
